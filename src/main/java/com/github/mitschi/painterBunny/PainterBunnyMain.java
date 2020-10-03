23307e2370da31ce4cdf6ed7bfc32551b4f6eff8
Christian Macho <christian.macho@gmx.at>
2020-10-03 23:51:49 +0200

    removed unnecessary array creation.

--- src/main/java/com/github/mitschi/painterBunny/PainterBunnyMain.java
+++ src/main/java/com/github/mitschi/painterBunny/PainterBunnyMain.java
@@ -13,7 +13,7 @@ public class PainterBunnyMain
         String name = (args.length>0 && args[0]!=null)?args[0]:"PainterBunny-"+ UUID.randomUUID();
         int timeForAnEgg = (args.length>1&&args[1]!=null)?Integer.parseInt(args[1]):(int)(1500+Math.random()*1000);
 
-        List<String> colors = Arrays.asList(new String[]{"red","green","blue","yellow","purple","black","white","orange"});
+        List<String> colors = Arrays.asList("red","green","blue","yellow","purple","black","white","orange");
         String color = colors.get((int) (Math.random()*colors.size()));
 
         PainterBunny emptierBunny = new PainterBunny(name,timeForAnEgg,color);
